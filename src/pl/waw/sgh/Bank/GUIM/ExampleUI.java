51e7cda1f6054daa4d4d8289cabd67a4cb92317d
rafald007 <rafald007@gmail.com>
2018-12-19 10:27:46 +0100

    GGUI

---
+++ src/pl/waw/sgh/Bank/GUIM/ExampleUI.java
@@ -0,0 +1,16 @@
+package pl.waw.sgh.Bank.GUIM;
+
+import javax.swing.*;
+
+public class ExampleUI {
+  private JPanel panel1;
+  private JButton addNewCustomerButton;
+  private JFormattedTextField formattedTextField1;
+  private JFormattedTextField formattedTextField2;
+  private JFormattedTextField formattedTextField3;
+  private JButton goToAccountActivitiesButton;
+
+  private void createUIComponents() {
+    // TODO: place custom component creation code here
+  }
+}
---
+++ src/pl/waw/sgh/Bank/GUIM/MonikaActivitiesBank.java
@@ -0,0 +1,11 @@
+package pl.waw.sgh.Bank.GUIM;
+
+import javax.swing.*;
+
+public class MonikaActivitiesBank {
+  private JFormattedTextField formattedTextField1;
+  private JFormattedTextField formattedTextField2;
+  private JButton submitChangesButton;
+  private JButton goBackToCustomerButton;
+  private JComboBox comboBox1;
+}
